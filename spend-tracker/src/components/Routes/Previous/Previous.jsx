b4eaf4400c5ee8e592996690e0c6a9dca94eb8bc
Friday Okorie <bitdoctrine@gmail.com>
2022-04-10 12:42:00 +0100

    just commiting

--- spend-tracker/src/components/Routes/Previous/Previous.jsx
+++ spend-tracker/src/components/Routes/Previous/Previous.jsx
@@ -2,35 +2,33 @@ import { useEffect, useState } from "react";
 import Header from "../../Header/Header";
 import "./Previous.css";
 
-
 function Previous(props) {
-
-  const [records, setRecords] = useState([]);
-
-  useEffect( () => {
+  useEffect(() => {
     fetchRecords();
   }, []);
 
+  const [records, setRecords] = useState([]);
 
-  const fetchRecords = async() => {
-    const data = await fetch('/previous');
+  const fetchRecords = async () => {
+    const data = await fetch("/previous");
     const items = await data.json();
     setRecords(items);
-    console.log(records)
-  }
-
-  
-
+    console.log(records);
+  };
 
   return (
-    <>
     <div className="main">
       <header className="App-header">
-        <Header  text="Spend Tracker"/>
-        {records.map(record => <div>{record.price}</div>)}
+        <Header text="Spend Tracker" />
+        {records.map((record) => (
+          <div className="entry">
+            <h1>{record.Date}</h1>
+            <h1>{record.name}</h1>
+            <h1>{record.price}</h1>
+          </div>
+        ))}
       </header>
-      </div>
-    </>
+    </div>
   );
 }
 export default Previous;
